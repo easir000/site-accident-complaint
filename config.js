7e07027fdfc2ce829ec4ed938a0e083667dad42c
saturated <46495303+easir000@users.noreply.github.com>
2022-05-09 17:15:27 +0600

    Update config.js
    ok

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  secretKey: "12345-67890-09876-54321",
+  secretKey: "",
   mongoUrl: "mongodb://127.0.0.1:27017/conFusion",
   cloudUrl:
-    "mongodb+srv://yuvrajmann:encyclopedi@cluster0.ow6fw.mongodb.net/conFusion?retryWrites=true&w=majority",
+    "",
 };
